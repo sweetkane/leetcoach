dd47ebda7582a823215513fd7a8b6aaa980dff03
Kane Sweet <kanesweet11@gmail.com>
2023-02-28 00:46:46 -0500

    boilerplate added

---
+++ main.js
@@ -0,0 +1,13 @@
+function myFunction() {
+    // get the button element
+    const button = document.getElementsByTagName("button")[0];
+
+    // create a new text node with the message
+    const message = document.createTextNode("Button clicked!");
+
+    // get the parent element of the button
+    const parent = button.parentNode;
+
+    // replace the button with the message
+    parent.replaceChild(message, button);
+  }
