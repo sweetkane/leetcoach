1dc529d6b042beb849b96015c638c48da54b0abb
sweetkane <kanesweet11@gmail.com>
2023-03-04 22:23:36 -0500

    aesthetic updates

--- scripts/interface.js
+++ scripts/interface.js
@@ -1,12 +1,10 @@
-//////////// messages for "api/v1/chat/completions" //////////////
-let chat = new chatmessages();
+const chat = new chatmessages();
 async function sendChatAndGetResponse(userChat) {
     if (chat.isnull) chat.init();
     chat.update_solution_progress();
     chat.add_user_message(userChat);
     const responseText = await prompt_chat(chat.messages);
     if (responseText === null) {
-        console.log("ERROR: got no response from Lambda");
         return "[ Error ] Please retry shortly";
     }
     chat.add_assistant_message(responseText);
@@ -16,10 +14,7 @@ async function sendChatAndGetResponse(userChat) {
 
 }
 
-
-// OPENAI API HANDLING
-// TODO start transition to gpt-3.5-turbo model
-// https://platform.openai.com/docs/guides/chat/introduction
+// Please do not try to exploit this lambda url. Thanks!
 async function prompt_chat(messages) {
     const url = "https://unsj2incakms24tnc3xhp4hk4y0gwivv.lambda-url.us-east-2.on.aws/"
     const response = await fetch(url,
@@ -27,14 +22,14 @@ async function prompt_chat(messages) {
             method: "POST",
             body: JSON.stringify(messages)
         }
-
     )
     if (response.ok) {
         const parsed = await response.json();
-        console.log("response:", parsed.response);
+        console.log("response good:", parsed.response);
         return parsed.response;
     }
     else {
+        console.log("response error:", response);
         return null;
     }
 }
